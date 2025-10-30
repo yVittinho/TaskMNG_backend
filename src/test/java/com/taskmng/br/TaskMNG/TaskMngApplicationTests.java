48016a4f289c935dcc2243a2389c78160229df78
Murillo Aragão <murilloaragaao@gmail.com>
2025-10-30 02:30:32 -0300

    java first commit

---
+++ src/test/java/com/taskmng/br/TaskMNG/TaskMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmng.br.TaskMNG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
