580722701259997ae0b397a1b1971bed68304ba6
Murillo Aragão <murilloaragaao@gmail.com>
2025-11-03 22:08:26 -0300

    aprimorando user service

---
+++ src/main/java/com/taskmng/br/TaskMNG/controllers/UsuarioController.java
@@ -0,0 +1,16 @@
+package com.taskmng.br.TaskMNG.controllers;
+
+
+import com.taskmng.br.TaskMNG.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+}
--- src/main/java/com/taskmng/br/TaskMNG/service/UsuarioService.java
+++ src/main/java/com/taskmng/br/TaskMNG/service/UsuarioService.java
@@ -1,22 +1,65 @@
 package com.taskmng.br.TaskMNG.service;
 
 import com.taskmng.br.TaskMNG.entities.Usuario;
+import com.taskmng.br.TaskMNG.enums.Perfil;
 import com.taskmng.br.TaskMNG.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 
 @Service
+@RequiredArgsConstructor
 public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    //public Usuario criarUsuario(Usuario usuario) {
+    public Usuario cadastrarUsuario(Usuario novoUsuario, Perfil perfilCriador) {
 
-        //if (usuarioRepository.existsByEmail(usuario.getEmail())) {
-            //throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "email já cadastrado.");
-        //}
+        if(perfilCriador != Perfil.ADMINISTRADOR && perfilCriador != Perfil.TECHLEAD){
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                    "apenas administradores ou techleads podem cadastrar usuários");
 
-    //}
+        }
+
+        if (usuarioRepository.existsByEmail(novoUsuario.getEmail())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "email já cadastrado.");
+
+        }
+
+        if(novoUsuario.getTipoPerfil() == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "perfil do usuário é obrigatório");
+        }
+
+        //lembrar de validar a senha com regex
+
+        //passando criptografia na senha para armazenar no banco
+        String senhaHash = new BCryptPasswordEncoder().encode(novoUsuario.getSenha());
+        novoUsuario.setSenha(senhaHash);
+
+        return usuarioRepository.save(novoUsuario);
+    }
+
+    // trazendo todos os usuarios
+    public List<Usuario> exibirUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+    // trazendo o usuario pelo id
+    public Usuario buscarPorId (Long id){
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "usuário não encontrado"));
+    }
+
+    //
+
+
 }
